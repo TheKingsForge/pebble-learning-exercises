0bca89b1367ef79dc25ac19a2c0704f7178785e8
TheKingsForge <zachking8@hotmail.com>
2025-12-03 13:15:29 -0600

    feat: Changed displayed text.

---
+++ 2-1_HelloPebble/build/aplite/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "HelloPebble",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_PLATFORM_APLITE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x8C, 0x2D, 0xA3, 0x2F, 0x94, 0x6D, 0x47, 0x34, 0xBC, 0xED, 0xF6, 0x82, 0x1D, 0x07, 0x64, 0xEE },
+  .virtual_size = 0xb6b6
+};
